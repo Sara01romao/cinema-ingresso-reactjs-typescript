869a737bf5246cb45500f3107007c50e3059613c
Sara Romao <sara-romao@live.com>
2024-09-27 22:33:02 -0300

    feat: melhorias no layout item ingresso e add btn remove

--- src/components/ticketCart/index.tsx
+++ src/components/ticketCart/index.tsx
@@ -1,4 +1,7 @@
 
+import { BiTrash } from 'react-icons/bi';
+import styles from './ticketCart.module.css' ;
+
 type Ticket = {
     seatId: string;
     type: 'inteira' | 'meia';
@@ -12,13 +15,25 @@ type Ticket = {
 
 
   export const TicketCart = ({ tickets }: TicketCartProps) => {
+     
+    
+
+    function handleRemoveTicket(id:string){
+        
+        alert(id)
+    }
+   
     return (
-      <div>
-        <h3>Ingressos Selecionados:</h3>
+      <div className={styles.containerTicketCart}>
+        <h3>Ingressos Selecionados</h3>
         <ul>
           {tickets.map((ticket, index) => (
-            <li key={index}>
-              Assento: {ticket.seatId} - {ticket.type} - R$ {ticket.price.toFixed(2)}
+            <li key={ticket.seatId}>
+              <p>Assento: <strong>{ticket.seatId} </strong></p>
+              <p>Tipo: <strong>{ticket.type}</strong>  R$ {ticket.price.toFixed(2)}</p>
+              <button onClick={() => handleRemoveTicket(ticket.seatId) }>
+                 <BiTrash size={20}/>
+              </button>
             </li>
           ))}
         </ul>
